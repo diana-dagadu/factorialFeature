2b41e4e4b5626e803d6e4ea9d9d31bcbef748c68
diana-dagadu <diana.dagadu@amalitech.org>
2021-08-19 10:09:30 +0000

    final commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const Calculate = {
+      factorial(input){
+      let fact = 1;
+      for (let i = 2; i <= input ; i++){
+        fact *= i;
+      }
+      return fact;
+    }
+}
+
+module.exports = Calculate;
